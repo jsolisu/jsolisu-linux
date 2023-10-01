2f53a97ca5f316065f9ea7ffd87115b809e2523d
Jean C. Solís <jcsolisu2000@gmail.com>
2023-10-01 16:18:59 -0600

    Cambiar comentarios incorrectos

--- git/optimize.sh
+++ git/optimize.sh
@@ -8,19 +8,19 @@ for dir in $dirlist; do
 
         echo -e "\e[31m\e[1mOptimizando \e[33m$dir\e[0m..."
 
-        // sincronizar contra las ramas remotas
+        # sincronizar contra las ramas remotas
         git remote update origin --prune
         
-        // quita las ramas que ya no existen remotamente, siempre que se hayan fusionado
+        # quita las ramas que ya no existen remotamente, siempre que se hayan fusionado
         git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
 
-        // limpia el registro de referencias
+        # limpia el registro de referencias
         git reflog expire --all --expire=now
 
-        // ejecuta el garbage collector
+        # ejecuta el garbage collector
         git gc --prune=now --aggressive
 
-        // quita los archivos que no estan bajo el control de versiones
+        # quita los archivos que no estan bajo el control de versiones
         git clean -ffd
     )
 done
