dde380e1f927a67d1c6ef6fee4530159e06a311c
Jean C. Solís <jcsolisu2000@gmail.com>
2024-05-04 10:52:32 -0600

    Agregar script para inicializar iptables

---
+++ network/iptables_reset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# IPv4
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
+# IPv6
+sudo ip6tables -P INPUT ACCEPT
+sudo ip6tables -P FORWARD ACCEPT
+sudo ip6tables -P OUTPUT ACCEPT
+sudo ip6tables -t nat -F
+sudo ip6tables -t mangle -F
+sudo ip6tables -F
+sudo ip6tables -X
+
+sudo netfilter-persistent save
