a297b22be415e1c26b89fd4bc128ae9243a39559
Jean C. Solís <jcsolisu2000@gmail.com>
2024-08-11 12:48:12 -0600

    Mejoras en borrado de branches que ya no existen en el repositorio remoto

--- git/optimize.sh
+++ git/optimize.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
 
+cleanup_deleted_branches() {
+    current_branch=$(git symbolic-ref --short HEAD)
+    branches_to_delete=$(git branch -vv | awk '/origin\/.*: gone]/ {print $1}')
+    default_local_branch=$(git remote show origin | awk '/HEAD branch:/ {print $NF}')
+
+    if [[ -n "$branches_to_delete" ]]; then
+        if echo "$branches_to_delete" | grep -q "^$current_branch\$"; then
+            echo "La rama actual ($current_branch) está en la lista de ramas para eliminar."
+
+            if [[ "$current_branch" == "$default_local_branch" ]]; then
+                echo "La rama actual es la rama por defecto local. Actualizando HEAD del remoto."
+
+                git remote set-head origin --auto
+                default_local_branch=$(git symbolic-ref --short refs/remotes/origin/HEAD | sed 's#refs/remotes/origin/##')
+
+                if [[ -n "$default_local_branch" ]]; then
+                    echo "Cambiando a la nueva rama por defecto local: $default_local_branch"
+                    git checkout $default_local_branch
+
+                    git branch -D $current_branch
+
+                    branches_to_delete=$(git branch -vv | awk '/origin\/.*: gone]/ {print $1}')
+                else
+                    echo "No se pudo determinar la nueva rama por defecto local."
+                    return 1
+                fi
+            else
+                echo "La rama actual no es la rama por defecto local."
+
+                if [[ -n "$default_local_branch" ]]; then
+                    echo "Cambiando a la rama por defecto local: $default_local_branch"
+                    git checkout $default_local_branch
+                else
+                    echo "No se pudo determinar la rama por defecto local."
+                    return 1
+                fi
+
+                git branch -D $current_branch
+
+                branches_to_delete=$(git branch -vv | awk '/origin\/.*: gone]/ {print $1}')
+            fi
+        fi
+
+        # Elimina las ramas locales que han sido eliminadas en el remoto
+        if [[ -n "$branches_to_delete" ]]; then
+            echo "Eliminando ramas locales que han sido eliminadas en el remoto:"
+            echo "$branches_to_delete" | xargs -r git branch -D
+        else
+            echo "No hay ramas locales para eliminar."
+        fi
+    else
+        echo "No hay ramas locales para eliminar."
+    fi
+}
+
 # Encuentra todos los directorios .git y cambia a sus directorios padres
 find . -type d -name '.git' -exec dirname {} \; | while IFS= read -r dir; do
     if [ -d "$dir" ]; then
         cd "$dir" || {
-            echo "No se pudo cambiar al directorio $dir"
+            echo "No se pudo cambiar al directorio [$dir]."
             continue
         }
 
-        echo -e "\e[31m\e[1mOptimizando \e[33m$dir\e[0m..."
+        echo -e "\e[31m\e[1mOptimizando \e[33m[$dir]\e[0m..."
 
         # Sincroniza las referencias locales con la información de del repositorio remoto
         echo -e "\e[32m\e[1mSincronizando ramas remotas\e[0m..."
@@ -16,7 +71,7 @@ find . -type d -name '.git' -exec dirname {} \; | while IFS= read -r dir; do
 
         # Elimina las ramas que ya no existen remotamente
         echo -e "\e[32m\e[1mQuitando ramas que ya no existen remotamente\e[0m..."
-        git branch -vv | awk '/origin\/.*: gone]/ {print $1}' | xargs -r git branch -D
+        cleanup_deleted_branches
 
         # Limpia el registro de referencias
         echo -e "\e[32m\e[1mLimpiando referencias\e[0m..."
@@ -32,10 +87,10 @@ find . -type d -name '.git' -exec dirname {} \; | while IFS= read -r dir; do
 
         # Regresa al directorio original
         cd - >/dev/null || {
-            echo "No se pudo regresar al directorio original"
+            echo "No se pudo regresar al directorio original."
             exit 1
         }
     else
-        echo "El directorio $dir no existe o no se puede acceder"
+        echo "El directorio $dir no existe o no se puede acceder."
     fi
 done
