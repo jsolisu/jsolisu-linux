34422daacd0ae75013e04a854b410200959bdc91
Jean C. Solís <jcsolisu2000@gmail.com>
2024-09-22 13:00:06 -0600

    Añadir script para clonar repositorios sin forks desde la API de GitHub

--- git/clone_nofork.sh
+++ git/clone_nofork.sh
@@ -19,14 +19,35 @@ while :; do
     # Obtener los repositorios de la página actual
     response=$(get_repos $page)
 
+    # Si la respuesta contiene la cadena "DOCTYPE", hemos llegado al final y puede pasar por un error
+    if [[ "$response" == *"DOCTYPE"* ]]; then
+        break
+    fi
+
     # Si la respuesta está vacía, hemos llegado al final
     if [[ -z "$response" || "$response" =~ ^\[[^[:alnum:]]*\]$ ]]; then
         break
     fi
 
-    # Filtrar los repositorios que no sean forks y obtener las URLs
-    echo "$response" | grep -E '"fork": false' -B 10 | grep -E '"html_url":' | awk -F '"' '{print $4}'
+    # Filtrar los repositorios que no sean forks y obtener los nombres y URLs
+    names=$(echo "$response" | grep -E '"fork": false' -B 30 | grep -E '"name":' | awk -F '"' '{print $4}')
+    urls=$(echo "$response" | grep -E '"fork": false' -B 10 | grep -E '"html_url":' | awk -F '"' '{print $4}')
+
+    # Clonar los repositorios
+    for name in $names; do
+        url=$(echo "$urls" | sed -n "1p")
+        urls=$(echo "$urls" | sed '1d')
+        
+        # Clonar solamente si el directorio $name no existe
+        if [ ! -d "$name" ]; then
+            echo "Clonando $name from $url"
+            git clone "$url"
+        fi
+    done
 
     # Incrementar el número de página
     page=$((page + 1))
+    
+    # Esperar 2 segundos para evitar exceder el límite de la API
+    sleep 2
 done
